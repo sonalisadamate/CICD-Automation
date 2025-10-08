8f8790a22ec7c80f68517b8de743f215d5f2f736
sonalisadamate <sonali.sadamate23@gmail.com>
2025-10-08 15:33:07 +0530

    adding updated class file

--- src/test/java/com/AutomationCICD/Project1/TestClass.java
+++ src/test/java/com/AutomationCICD/Project1/TestClass.java
@@ -1,14 +1,12 @@
 package com.AutomationCICD.Project1;
 
-import java.io.File;
 import java.time.Duration;
 
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -18,65 +16,45 @@ public class TestClass {
     public WebDriver driver;
 
     @BeforeMethod
-    public void launchDriver() {
-        try {
-            // Setup ChromeDriver using WebDriverManager
-            WebDriverManager.chromedriver().setup();
+    public void setup() {
+        // Setup ChromeDriver using WebDriverManager
+        WebDriverManager.chromedriver().setup();
 
-            ChromeOptions options = new ChromeOptions();
+        // Create ChromeOptions
+        ChromeOptions options = new ChromeOptions();
 
-            // Explicit Chrome binary location
-            options.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
+        // Run in headless mode
+        options.addArguments("--headless=new"); // "new" headless mode for Chrome 109+
 
-            // Headless + safe Jenkins options
-            options.addArguments("--headless=new");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--disable-gpu");
-            options.addArguments("--disable-extensions");
-            options.addArguments("--remote-allow-origins=*");
-            options.addArguments("--window-size=1920,1080");
+        // Disable GPU, extensions, notifications
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-notifications");
 
-            // Jenkins-friendly temp directories
-            String tempDir = "C:/Temp/JenkinsChrome";
-            File tempFolder = new File(tempDir);
-            if (!tempFolder.exists()) {
-                tempFolder.mkdirs();
-            }
-            options.addArguments("--user-data-dir=" + tempDir + "/ChromeData");
-            options.addArguments("--disk-cache-dir=" + tempDir + "/ChromeCache");
+        // Use dedicated temp folder for Jenkins (SYSTEM-friendly)
+        options.addArguments("--user-data-dir=C:/Temp/JenkinsChrome");
+        options.addArguments("--disk-cache-dir=C:/Temp/JenkinsChrome/Cache");
 
-            // Initialize ChromeDriver
-            driver = new ChromeDriver(options);
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.manage().window().setSize(new Dimension(1920, 1080));
+        // Optional: Prevent sandbox issues for SYSTEM account
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("Failed to launch ChromeDriver on Jenkins!");
-        }
+        // Initialize driver
+        driver = new ChromeDriver(options);
+
+        // Maximize or set window size (optional)
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @Test
-    public void Test1() {
-        driver.navigate().to("https://www.automationtalks.com");
-        System.out.println("Test 1 title is " + driver.getTitle());
-    }
-
-    @Test
-    public void Test2() {
-        driver.navigate().to("https://www.automationtalks.com");
-        System.out.println("Test 2 title is " + driver.getTitle());
-    }
-
-    @Test
-    public void Test3() {
-        driver.navigate().to("https://www.automationtalks.com");
-        System.out.println("Test 3 title is " + driver.getTitle());
+    public void openGoogle() {
+        driver.get("https://www.google.com");
+        System.out.println("Page title: " + driver.getTitle());
     }
 
     @AfterMethod
-    public void tearDown() {
+    public void teardown() {
         if (driver != null) {
             driver.quit();
         }
